c3b3df5b43422c1c3a4ab16cff4e3955b36e5aa4
Shahzeb Ahmad <44709834+shahzeb99@users.noreply.github.com>
2020-10-21 19:45:23 +0530

    Update traversal.py

--- Tree/traversal.py
+++ Tree/traversal.py
@@ -56,11 +56,11 @@ def printPreorder(root):
 
 
 # Driver code 
-root = Node(1) 
-root.left	 = Node(2) 
-root.right	 = Node(3) 
-root.left.left = Node(4) 
-root.left.right = Node(5) 
+root = Node(6) 
+root.left	 = Node(7) 
+root.right	 = Node(8) 
+root.left.left = Node(9) 
+root.left.right = Node(10) 
 print "Preorder traversal of binary tree is"
 printPreorder(root) 
 
